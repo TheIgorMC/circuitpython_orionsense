7464d159d78e3352a8bb8785a0def6a384f6dee0
Giovanni Cesarotto <72604500+TheIgorMC@users.noreply.github.com>
2025-12-15 12:40:28 +0100

    Updated pinouts

--- ports/nordic/boards/OrionTrainerV2/mpconfigboard.h
+++ ports/nordic/boards/OrionTrainerV2/mpconfigboard.h
@@ -1,33 +1,17 @@
 // mpconfigboard.h
-
 #pragma once
 
-#define MICROPY_HW_BOARD_NAME       "ArcheryTrainer V2"
+#define MICROPY_HW_BOARD_NAME       "Archery Trainer V2"
 #define MICROPY_HW_MCU_NAME         "nRF52840"
 
-// --- FIX BLUETOOTH (Già discusso) ---
+// --- FIX BLUETOOTH (Fondamentale) ---
 #define BOARD_HAS_32KHZ_XTAL        (0)
 
 // --- LED DI STATO ---
 #define MICROPY_HW_LED_STATUS       (&pin_P0_22)
 #define MICROPY_HW_LED_STATUS_INVERTED (1)
 
-// --- CONFIGURAZIONE USB (NUOVA SEZIONE) ---
-// Questi ID dicono al PC che siamo un dispositivo CircuitPython generico.
-// Se usi ID a caso, Windows potrebbe non caricare i driver automaticamente.
-#define MICROPY_HW_USB_VID          (0x239A) // Adafruit / CircuitPython VID
-#define MICROPY_HW_USB_PID          (0x8029) // Generic nRF52840 CircuitPython PID
-
-#define MICROPY_HW_USB_MANUFACTURER "TuoNome"
-#define MICROPY_HW_USB_PRODUCT      "Archery Trainer"
-
-// --- MEMORIA FLASH (Opzionale ma consigliato) ---
-// Definisce quanto spazio dedicare al disco "CIRCUITPY".
-// L'nRF52840 ha 1MB. Lasciamo spazio per bootloader e firmware,
-// il resto va al filesystem. (0 = Auto-detect massimo possibile)
-#define CIRCUITPY_INTERNAL_FLASH_FILESYSTEM_SIZE (0)
-
-// --- PIN DI DEFAULT PER I BUS ---
+// --- PIN DI DEFAULT BUS ---
 #define DEFAULT_I2C_BUS_SCL         (&pin_P0_27)
 #define DEFAULT_I2C_BUS_SDA         (&pin_P0_26)
 
--- ports/nordic/boards/OrionTrainerV2/mpconfigboard.mk
+++ ports/nordic/boards/OrionTrainerV2/mpconfigboard.mk
@@ -1,8 +1,22 @@
+# mpconfigboard.mk
+
+# VID Adafruit (standard per CircuitPython)
 USB_VID = 0x239A
-USB_PID = 0x80BC
-USB_PRODUCT = "MDBT50Q-DB-40"
-USB_MANUFACTURER = "Raytac Corporation"
 
+# PID Generico per nRF52840 (0x8029) invece di quello Raytac DB-40 (0x80BC)
+# Questo aiuta Windows a capire che è un dispositivo generico CircuitPython
+USB_PID = 0x8029
+
+# I nomi che appariranno su Windows/Mac/Linux
+USB_PRODUCT = "Archery Trainer V2"
+USB_MANUFACTURER = "Orion Electronics"
+
+# Configurazione Chip
 MCU_CHIP = nrf52840
+MCU_SUB_VARIANT = nrf52840
 
+# Abilita il filesystem (La chiavetta CIRCUITPY)
 INTERNAL_FLASH_FILESYSTEM = 1
+
+# Ottimizzazione (Opzionale, di solito si lascia default)
+# LONGINT_IMPL = MPZ
